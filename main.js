0556f525211118423aad26f8eb938a6e8e98571d
TheBestNinja <judgsmith@gmail.com>
2023-07-01 07:02:44 -0500

    optimizing for mobile

--- main.js
+++ main.js
@@ -13,33 +13,11 @@ for(let i = 0; i != data.length; i++){
     let card = " <a href='"+ data[i].url +"'> <div class='card-main d-flex row align-items-start'>  <div class='card-img-top  d-flex' style='background: url("+ data[i].image +");'> <h1 class='card-title  d-flex'>" + data[i].title + "</h1></div> <div class='card-body  d-flex'> <p class='card-text  d-flex'>"+ data[i].description + "</p> </div> </div> </a> "
   
     if(type == data[i].type){
-      
-        if(counter== 1){
-            cardsToShow += "<div class='cards d-flex flex-wrap justify-content-between container'>";
-            cardsToShow += card
-            counter++
-        }
-        else if(counter == 2){
-            cardsToShow += card
-            cardsToShow += "</div> <br>"; 
-            counter = 1;
-        }
-        
+        cardsToShow += card
     }
 
     if(type == "all"){
-
-        if(counter== 1){
-            cardsToShow += "<div class='cards d-flex flex-wrap justify-content-between container'>";
-            cardsToShow += card
-            counter++
-        }
-        else if(counter == 2){
-            cardsToShow += card
-            cardsToShow += "</div> <br>"; 
-            counter = 1;
-        }
-        
+            cardsToShow += card   
     }
    
 }
